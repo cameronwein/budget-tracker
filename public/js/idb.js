8ee8b028aa46ee5ab0eab0f5ce3157762eb7ca07
Cameron Wein <cameron.wein@gmail.com>
2022-01-07 09:21:39 -0600

    add offline database

---
+++ public/js/idb.js
@@ -0,0 +1,67 @@
+let db;
+const request = indexedDB.open('budget_tracker', 1);
+
+request.onupgradeneeded = function(event) {
+    const db = event.target.result;
+    db.createObjectStore('budget_tracker', { autoIncrement: true });
+};
+
+request.onsuccess = function(event) {
+    db = event.target.result;
+  
+    if (navigator.onLine) {
+      uploadTransactions();
+    }
+  };
+  
+  request.onerror = function(event) {
+    console.log(event.target.errorCode);
+};
+
+
+function saveRecord(record) {
+    const transaction = db.transaction(['budget_tracker'], 'readwrite');
+  
+    const budgetObjectStore = transaction.objectStore('budget_tracker');
+  
+    budgetObjectStore.add(record);
+}
+
+function uploadTransactions() {
+    const transaction = db.transaction(['budget_tracker'], 'readwrite');
+  
+    const budgetObjectStore = transaction.objectStore('budget_tracker');
+  
+    const getAll = budgetObjectStore.getAll();
+  
+    getAll.onsuccess = function() {
+        if (getAll.result.length > 0) {
+        fetch('/api/transaction/bulk', {
+            method: 'POST',
+            body: JSON.stringify(getAll.result),
+            headers: {
+            Accept: 'application/json, text/plain, */*',
+            'Content-Type': 'application/json'
+            }
+        })
+            .then(response => response.json())
+            .then(serverResponse => {
+            if (serverResponse.message) {
+                throw new Error(serverResponse);
+            }
+            const transaction = db.transaction(['budget_tracker'], 'readwrite');
+            const budgetObjectStore = transaction.objectStore('budget_tracker');
+            budgetObjectStore.clear();
+
+            alert('All saved transactions have been submitted!');
+            })
+            .catch(err => {
+            console.log(err);
+            });
+        }
+    };
+}
+
+// listen for app coming back online
+window.addEventListener('online', uploadTransactions);
+
